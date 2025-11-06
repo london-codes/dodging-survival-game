fb308af559fdc32784dfba18f3abf11ce6efde59
london-codes <london.powell.work@gmail.com>
2025-11-06 15:28:32 -0800

    intial commit

---
+++ dodging survival game.cpp
@@ -0,0 +1,39 @@
+// dodging survival game.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <SFML/OpenGL.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <array>
+
+int main()
+{
+    auto window = sf::RenderWindow(sf::VideoMode({ 1920u, 1080u }), "Spcae dodger");
+    window.setFramerateLimit(144);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
