d5b38aa124dc77f5a5b3b909145743c5368d1e97
london-codes <london.powell.work@gmail.com>
2025-11-16 15:50:24 -0800

    basic pause implmeneted

--- main.cpp
+++ main.cpp
@@ -10,72 +10,92 @@
 
 int main()
 {
+
     auto window = sf::RenderWindow(sf::VideoMode({ 1600u, 900u }), "Spcae dodger");
     window.setFramerateLimit(240);
 
-    PlayerShip player;
-
-    Meteor meteor;
-
-    Physics physics;
-
-    sf::Clock clock;
- 
-
-    while (window.isOpen())
+    // loop for a restart
+    bool paused{ false };
+    bool running{ true };
+    while (running)
     {
+        PlayerShip player;
 
-        while (const std::optional event = window.pollEvent())
+        Meteor meteor;
+
+        Physics physics;
+
+        sf::Clock clock;
+
+
+        while (window.isOpen())
         {
-            if (event->is<sf::Event::Closed>())
+
+            while (const std::optional event = window.pollEvent())
             {
-                window.close();
+                if (event->is<sf::Event::Closed>())
+                {
+                    window.close();
+                }
+                
+                if (event->is<sf::Event::KeyPressed>() &&
+                    event->getIf<sf::Event::KeyPressed>()->code == sf::Keyboard::Key::Escape)
+                {
+                    paused = !paused;
+                    // Do something once when Escape is pressed
+                    // For example: pause the game or open a menu
+                    
+                }
+
             }
+
+            // time mechanics
+            float dt = clock.restart().asSeconds();
+
+            if (paused)
+                continue;
+
+            // inputs
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+            {
+                player.forwardPropulsion(dt);
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+            {
+                player.rotateRight(dt);
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+            {
+                player.rotateLeft(dt);
+            }
+
+
+            // updates
+            player.update(dt);
+            meteor.update(dt);
+
+
+            // collisions
+            if (player.getGlobalPos().findIntersection(meteor.getGlobalPos()))
+            {
+                physics.collision(player, meteor);
+
+                //meteor.collision(player.getMass(), player.getVelocity());
+                //player.collision(meteor.getMass(), meteor.getVelocity());
+            }
+
+
+            // render
+            window.clear(sf::Color(25, 25, 112));
+            player.draw(window);
+            meteor.draw(window);
+            window.display();
         }
-
-        // time mechanics
-        float dt = clock.restart().asSeconds();
-
-        // inputs
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
-        {
-            player.forwardPropulsion(dt);
-        }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
-        {
-            player.rotateRight(dt);
-        }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
-        {
-            player.rotateLeft(dt);
-        }
-
-
-        // updates
-        player.update(dt);
-        meteor.update(dt);
-
-
-        // collisions
-        if (player.getGlobalPos().findIntersection(meteor.getGlobalPos()))
-        {
-            physics.collision(player, meteor);
-
-            //meteor.collision(player.getMass(), player.getVelocity());
-            //player.collision(meteor.getMass(), meteor.getVelocity());
-        }
-    
-
-        // render
-        window.clear(sf::Color(25, 25, 112));
-        player.draw(window);
-        meteor.draw(window);
-        window.display();
     }
 }
-
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
